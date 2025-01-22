e1fd6db3f054940ee356041f591c605b5137d2d2
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-22 16:16:32 +0600

    footer fixed

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return(
         <footer className="bg-gray-200 p-4 text-center">
             <p>
-                © {new Date().getFullYear()} Book E-commerce. All rights reserved.
+                © {new Date().getFullYear()} 99BOOKS.All Rights Reserved.
             </p>
 
         </footer>
